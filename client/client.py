d4c335d8dd17716c6bd092caf416587966eafc55
samuel <dalin.samuel@gmail.com>
2022-10-20 21:47:33 +0200

    Dodawanie sidebar

--- client/client.py
+++ client/client.py
@@ -1,4 +1,3 @@
-from email.mime import image
 import socket
 import pygame as pg
 import json
@@ -7,9 +6,20 @@ from screeninfo import get_monitors
 import time
 import math
 
+pg.init()
+pg.font.init()
+
 black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
+gray = (150,150,150)
+
+screen_width = 1000
+screen_height = 1000
+
+side_bar_width = 100
+font_size = 20
+my_font = pg.font.SysFont("Arial",font_size)
 
 def draw_map(screen, screen_dimensions, board, player, size_square):
 
@@ -50,20 +60,27 @@ def login():
             return nick,got["board"],got["square_size"], handler
         except:
             print("Ten nick jest juz w grze")
-    
+
+def draw_side_bar(screen,player):
+    pg.draw.rect(screen,gray,(screen_width,0,side_bar_width,screen_height))
+
+    text_surface_level = my_font.render("Poziom: " + str(player["level"]),False,black)
+    text_surface_hp = my_font.render("HP: " + str(player["life_points"]),False,black)
+
+
+    gap = 10
+
+    screen.blit(text_surface_level,(screen_width + 10,150))
+    screen.blit(text_surface_hp,(screen_width + 10,150 + font_size + gap))
 
 
 
 def main():
 
-    screen_width = 1000
-    screen_height = 1000
-    screen = pg.display.set_mode((screen_width,screen_height))
-
     nick,board,square_size,handler = login()
-    pg.init()
 
     img_player = pg.image.load("./images/player_img.png")
+    screen = pg.display.set_mode((screen_width + side_bar_width,screen_height))
 
     running = True
     while running:
@@ -87,10 +104,19 @@ def main():
         direction = (x_mouse - screen_width / 2, y_mouse - screen_height / 2)
         direction = normalize_vector(direction)
 
+
+        # To jest sprawdzanie czy pozycja myszki nie jest w side_bar.
+        if pg.mouse.get_pos()[0] < screen_width:
+            mouse_is_pressed = [int(pg.mouse.get_pressed(num_buttons=3)[0]),int(pg.mouse.get_pressed(num_buttons=3)[1]),int(pg.mouse.get_pressed(num_buttons=3)[2])]
+        else:
+            mouse_is_pressed = [0,0,0]
+
+        
+
         handler.send_data({
             "arrows_pressed": [keys_pressed[pg.K_UP],keys_pressed[pg.K_DOWN],keys_pressed[pg.K_LEFT],keys_pressed[pg.K_RIGHT]],
             "direction": direction,
-            "mouse_pressed": pg.mouse.get_pressed(num_buttons=3),
+            "mouse_pressed": mouse_is_pressed,
             "extra_info": []
         })
 
@@ -107,6 +133,10 @@ def main():
         # Wyswietlamy playerow
         for nick,i in players.items():
             screen.blit(img_player,(change_cordinate(screen_width,player["FOV_x"],i["position_x"],player["position_x"]) - img_player.get_width() / 2,change_cordinate(screen_width,FOV_y,i["position_y"],player["position_y"]) - img_player.get_height() / 2))
+
+
+        # Wyswietlamy sidebar
+        draw_side_bar(screen,player)
 
         pg.display.flip()
 
